9c9762dee6c5596e8dd0beecef47371a2c1d7589
Balfegoru <73392021+Balfegoru@users.noreply.github.com>
2022-04-30 19:39:53 +0300

    Add files via upload

---
+++ Lesson2/EX2.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+
+void NumberSimple(int number)
+{
+   
+    for (int i = 2; i < number; i ++ )
+    {
+        
+        if (number % i == 0)
+        {
+            printf("Not Simple Number");
+            return;
+        }
+    }
+
+    printf("Simple Number");
+
+}
+
+
+int main()
+{
+
+    NumberSimple(10);
+
+    return 0;
+}
+
